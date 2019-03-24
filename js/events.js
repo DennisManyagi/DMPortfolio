49ccae90f4b57c5c402a037c9a0a490f3baa8b82
Dennis Manyagi <manyagi.dennis@gmail.com>
2019-03-24 01:43:12 -0700

    json added, alignment on pages fixed

--- js/events.js
+++ js/events.js
@@ -4,60 +4,50 @@ xhr.onload = function() {
     //save JSON data
     let data = JSON.parse(xhr.responseText);
 
-    //Insert the HTML articles & into section ( insertAdjacentHTML)
+    console.log(data);
 
+
+
+    //Insert the HTML articles & into section ( insertAdjacentHTML)
     let popularSection = document.querySelector('#popular');
     if (popularSection){
 
         //create variable to store data
-        let comics = '';
+        let popular = '';
 
 
-        //loop through each JSON object
-        for (let i = 0; i < data.length; i++){
+        //loop through data and place list titles
+    for (let i=0; i < data.length; i++) {
 
 
-            //format HTML with encapsulated variables
-            comics += '<article>';
-            comics += '<p class = "thumbnail"><img src =" '+ data.comics[i].image + '"alt="' + data.comics[i].location+ '"></p>';
+        //format HTML with encapsulated variables
+        popular += '<div class="col-sm-6">';
+        popular += '<section class="card mb-5">';
 
-            comics += '<h3>' + data.comics[i].location + '</h3>';
-            comics += '<h4 class = "location">' + data.comics[i].city+','+ data.comics[i].state + '</h4>';
-            comics += '<p>'+data.comics[i].description+'</p>';
-            comics += '<p><meter min="0" max ="5" value ="'+data.comics[i].rating +'">' + data.comics[i].rating+ 'out of 5</meter><a href="/rate">Rate it</a></p>';
-
-
-            comics += '<dl>';
-
-            comics += '<dt>Volume</dt>';
-            comics += '<dd>'+data.comics[i].volume +   '</dd>'
+        popular += '<img class="card-img-top img-fluid" src="'+data.homes[i].image+'" href="#">';
+        popular += '<div class="card-header">';
+        popular += '<h2 class="card-title">'+data.homes[i].city+'</h2>';
+        popular += '</div>';
 
 
-            comics += '<dt>Issue</dt>';
-            comics += '<dd>'+data.comics[i].issue +   '</dd>'
+        popular += '<div class="card-footer">';
 
-            comics += '<dt>Release Date</dt>';
-            comics += '<dd>'+data.comics[i].release+   '</dd>'
+        popular += '<h5 class="card-subtitle">'+data.homes[i].location+'</h5>';
+        popular += '</div>'
 
-            comics += '<dt>Read More</dt>';
-            comics += '<dd><p><a href="'+data.comics[i].link+'" target="_blank">View Info</a></p></dd>'
+
+        popular += '</section>';
+        popular += '</div>'
+        //--Card end HTML-->
 
 
 
 
-            comics += '</dl>';
+    }
 
 
 
-            comics += '</article>';
-
-
-
-        }
-
-
-
-        comicsSection.querySelector('h2').insertAdjacentHTML('afterend', comics);
+        popularSection.insertAdjacentHTML("afterend", popular);
 
 
 
